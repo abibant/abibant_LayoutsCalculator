91c47c9e94c4def79309bc9849d87bf47b9e322e
mayl-world <qwertyyyy65@yahoo.com>
2026-05-30 00:04:25 -0500

    Add project files.

---
+++ abibant_LayoutsCalculator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace abibant_LayoutsCalculator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
